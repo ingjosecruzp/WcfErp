5ea9df14aedc2b7db1267d9409231811b8fdd0c8
Germtz <ger.ms19@gmail.com>
2018-10-01 16:35:39 -0600

    cambios

--- WcfErp/Modelos/Generales/SubgrupoComponente.cs
+++ WcfErp/Modelos/Generales/SubgrupoComponente.cs
@@ -5,7 +5,9 @@ using System.Web;
 
 namespace WcfErp.Modelos.Generales
 {
-    public class SubgrupoComponente
+    public class SubgrupoComponente : ModeloBase
     {
+        public string Nombre { get; set; }
+        public GrupoComponente GrupoComponente { get; set; }
     }
 }
