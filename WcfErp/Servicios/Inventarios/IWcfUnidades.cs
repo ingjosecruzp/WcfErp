ec72c2e12f670c4bc538d6aa08fdd106778b78d2
TonyPlata <43586730+TonyPlata@users.noreply.github.com>
2018-10-01 17:58:14 -0500

    Cambios unidad

--- WcfErp/Servicios/Inventarios/IWcfUnidades.cs
+++ WcfErp/Servicios/Inventarios/IWcfUnidades.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using WcfErp.Modelos.Generales;
 
 namespace WcfErp.Servicios.Inventarios
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IWcfUnidades" in both code and config file together.
     [ServiceContract]
-    public interface IWcfUnidades
+    public interface IWcfUnidades : ServiciosBase<Unidad>
     {
-        [OperationContract]
-        void DoWork();
+       
     }
 }
--- WcfErp/Servicios/Inventarios/WcfUnidades.svc.cs
+++ WcfErp/Servicios/Inventarios/WcfUnidades.svc.cs
@@ -4,15 +4,42 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using WcfErp.Modelos.Generales;
 
 namespace WcfErp.Servicios.Inventarios
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WcfUnidades" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select WcfUnidades.svc or WcfUnidades.svc.cs at the Solution Explorer and start debugging.
-    public class WcfUnidades : IWcfUnidades
+    public class WcfUnidades : ServiceBase<Unidad> ,IWcfUnidades
     {
+        public Unidad add(Unidad item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Unidad> all()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Unidad delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
         public void DoWork()
         {
+        }
+
+        public Unidad get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Unidad update(Unidad item, string id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
