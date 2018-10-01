24d1edf0e64b43663596983c7014a78a6d72a468
ingjosecruzp <ing.josecruzp@gmail.com>
2018-10-01 16:49:12 -0600

    Concepto

--- WcfErp/Servicios/Inventarios/IWcfConceptos.cs
+++ WcfErp/Servicios/Inventarios/IWcfConceptos.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using WcfErp.Modelos.Inventarios;
 
 namespace WcfErp.Servicios.Inventarios
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IWcfConceptos" in both code and config file together.
     [ServiceContract]
-    public interface IWcfConceptos
+    public interface IWcfConceptos : ServiciosBase<Concepto>
     {
-        [OperationContract]
-        void DoWork();
+      
     }
 }
--- WcfErp/Servicios/Inventarios/WcfConceptos.svc.cs
+++ WcfErp/Servicios/Inventarios/WcfConceptos.svc.cs
@@ -4,15 +4,37 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using WcfErp.Modelos.Inventarios;
 
 namespace WcfErp.Servicios.Inventarios
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WcfConceptos" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select WcfConceptos.svc or WcfConceptos.svc.cs at the Solution Explorer and start debugging.
-    public class WcfConceptos : IWcfConceptos
+    public class WcfConceptos : ServiceBase<Concepto>,IWcfConceptos
     {
-        public void DoWork()
+        public Concepto add(Concepto item)
         {
+            throw new NotImplementedException();
+        }
+
+        public List<Concepto> all()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Concepto delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Concepto get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Concepto update(Concepto item, string id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
