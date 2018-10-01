72407ade234f8d509c2d7ebe3fa71abca0eeb743
ingjosecruzp <ing.josecruzp@gmail.com>
2018-10-01 16:34:07 -0600

    Nuevos Catalogos

---
+++ WcfErp/Modelos/Generales/TipoComponente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfErp.Modelos.Generales
+{
+    public class TipoComponente : ModeloBase
+    {
+        public string Nombre { get; set; }
+    }
+}
--- WcfErp/Modelos/Inventarios/Concepto.cs
+++ WcfErp/Modelos/Inventarios/Concepto.cs
@@ -5,7 +5,16 @@ using System.Web;
 
 namespace WcfErp.Modelos.Inventarios
 {
-    public class Concepto
+    public class Concepto : ModeloBase
     {
+        public string Clave { get; set; }
+        public string FolioAutomatico { get; set; }
+        public string Nombre{ get; set; }
+        public string Naturaleza { get; set; }
+
+        public TipoConcepto TipoConcepto { get; set; }
+        public string Predefinido { get; set; }
+
+        public string CostoAutomatico { get; set; }
     }
 }
