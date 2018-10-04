b8950b83e32b2155dd779b2419f3c5ecd9e0021b
Fernando Vazquez <fernando_keru1@hotmail.com>
2018-10-04 14:25:47 -0600

    TipoComponente Service

---
+++ WcfErp/Servicios/Inventarios/WcfTipoComponente.svc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WcfErp.Modelos.Generales;
+
+namespace WcfErp.Servicios.Inventarios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "WcfTipoComponente" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione WcfTipoComponente.svc o WcfTipoComponente.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class WcfTipoComponente : ServiceBase<TipoComponente>, IWcfTipoComponente
+    {
+        public TipoComponente delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TipoComponente get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TipoComponente update(TipoComponente item, string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
