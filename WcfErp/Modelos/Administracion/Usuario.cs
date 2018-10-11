3b3f5ec6bda04bbc83eec115d582e5df82264acc
Fernando Vazquez <fernando_keru1@hotmail.com>
2018-10-11 14:05:57 -0600

    WcfUsuario/Administacion WcfUsuarioRol/Administracion Modelo UsuarioRol/Administacion Modelo Usuario/Administracion Declarado Web.config Servicios y modelos

---
+++ WcfErp/Modelos/Administracion/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfErp.Modelos.Administracion
+{
+    public class Usuario : ModeloBase
+    {
+        public string Nombre { get; set; }
+        public string Abreviatura { get; set; }
+    }
+}
---
+++ WcfErp/Servicios/Administracion/IWcfUsuarioRol.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfErp.Servicios.Administracion
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IWcfUsuarioRol" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IWcfUsuarioRol
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
