0695c82038d0a62c2ee17cca4280c6a68ae5c354
Fernando Vazquez <fernando_keru1@hotmail.com>
2018-10-15 12:42:02 -0600

    WcfAlmacenes hecho

--- WcfErp/Servicios/Inventarios/IWcfAlmacenes.cs
+++ WcfErp/Servicios/Inventarios/IWcfAlmacenes.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using WcfErp.Modelos.Generales;
 
 namespace WcfErp.Servicios.Inventarios
 {
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IWcfAlmacenes" en el código y en el archivo de configuración a la vez.
     [ServiceContract]
-    public interface IWcfAlmacenes
+    public interface IWcfAlmacenes : ServiciosBase<Almacen>
     {
-        [OperationContract]
-        void DoWork();
+
     }
+    
 }
