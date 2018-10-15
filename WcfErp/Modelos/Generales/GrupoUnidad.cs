7d6cf4ecd215b8216eb15c6f0c0b1aa358f94503
ingjosecruzp <ing.josecruzp@gmail.com>
2018-10-15 16:41:15 -0600

    grupo Unidades

---
+++ WcfErp/Modelos/Generales/GrupoUnidad.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfErp.Modelos.Generales
+{
+    public class GrupoUnidad : ModeloBase
+    {
+        public string Nombre { get; set; }
+        public List<GrupoUnidadDetalle> GrupoUnidadDetalle { get; set; }
+    }
+}
---
+++ WcfErp/Modelos/Generales/GrupoUnidadDetalle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfErp.Modelos.Generales
+{
+    public class GrupoUnidadDetalle 
+    {
+        public double CantidadEquivalente { get; set; }
+        public Unidad UnidadEquivalente { get; set; }
+        public double CantidadBase { get; set; }
+        public Unidad UnidadBase { get; set; }
+    }
+}
