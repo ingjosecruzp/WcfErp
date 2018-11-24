85be251cf5887e6fffb6ba4d40c131fe062f4946
Germtz <ger.ms19@gmail.com>
2018-11-23 18:05:04 -0700

    purezas

---
+++ WcfErp/Modelos/Generales/Pureza.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WcfErp.Modelos.Generales
+{
+    public class Pureza : ModeloBase
+    {
+        [BsonRequired]
+        public string Nombre { get; set; }
+        [BsonRequired]
+        public GrupoComponente GrupoComponente { get; set; }
+    }
+}
---
+++ WcfErp/Servicios/Inventarios/IWcfPureza.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WcfErp.Modelos.Generales;
+using System.ServiceModel.Web;
+
+namespace WcfErp.Servicios.Inventarios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IWcfPureza" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IWcfPureza : ServiciosBase<Pureza>
+    {
+        [OperationContract]
+        [WebInvoke(UriTemplate = "?searchBy=getXGrupo&busqueda={busqueda}&_id={_id}",
+           BodyStyle = WebMessageBodyStyle.Bare,
+           ResponseFormat = WebMessageFormat.Json,
+           RequestFormat = WebMessageFormat.Json,
+           Method = "GET")]
+        List<Pureza> searchXGrupo(string busqueda, string _id);
+    }
+}
