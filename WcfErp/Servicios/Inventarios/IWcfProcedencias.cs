40e2e5ddd970c3e065b4e0bcd443703ef2a106d9
Germtz <ger.ms19@gmail.com>
2018-12-06 10:08:53 -0700

    Servicio

---
+++ WcfErp/Servicios/Inventarios/IWcfProcedencias.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WcfErp.Modelos.Inventarios;
+
+namespace WcfErp.Servicios.Inventarios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IWcfProcedencias" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IWcfProcedencias : ServiciosBase<Procedencia>
+    {
+        
+    }
+}
---
+++ WcfErp/Servicios/Inventarios/WcfProcedencias.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WcfErp.Modelos.Inventarios;
+
+namespace WcfErp.Servicios.Inventarios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "WcfProcedencias" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione WcfProcedencias.svc o WcfProcedencias.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class WcfProcedencias : ServiceBase<Procedencia>, IWcfProcedencias
+    {
+        public Procedencia delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
