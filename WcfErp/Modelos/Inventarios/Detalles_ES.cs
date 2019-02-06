1d0071eb93d9a7c5bb9aefb928bf65a19876f2e5
ingjosecruzp <ing.josecruzp@gmail.com>
2019-02-05 19:13:00 -0700

    Cambios movimientos entradas es

---
+++ WcfErp/Modelos/Inventarios/Detalles_ES.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WcfErp.Modelos.Generales;
+
+namespace WcfErp.Modelos.Inventarios
+{
+    public class Detalles_ES
+    {
+        public Articulo Articulo { get; set; }
+        public double Cantidad { get; set; }
+        public string Clave { get; set; }
+        public double Costo { get; set; }
+        public Unidad Unidad { get; set; }
+        public double CostoTotal { get; set; }
+    }
+}
