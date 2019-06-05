a4ad4d71a6b8d3dff4154bd7d14675c21f8de921
Germtz <ger.ms19@gmail.com>
2019-06-05 17:58:10 -0600

    monedas

--- WcfErp/Servicios/Generales/WcfMoneda.svc.cs
+++ WcfErp/Servicios/Generales/WcfMoneda.svc.cs
@@ -12,27 +12,7 @@ namespace WcfErp.Servicios.Generales
     // NOTE: In order to launch WCF Test Client for testing this service, please select WcfMoneda.svc or WcfMoneda.svc.cs at the Solution Explorer and start debugging.
     public class WcfMoneda : ServiceBase<Moneda>, IWcfMoneda
     {
-        public Moneda add(Moneda item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Moneda> all()
-        {
-            throw new NotImplementedException();
-        }
-
         public Moneda delete(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Moneda get(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Moneda update(Moneda item, string id)
         {
             throw new NotImplementedException();
         }
