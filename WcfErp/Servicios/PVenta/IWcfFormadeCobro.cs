cd25b2165d86aa2f0799e9c6b87b26640defa993
Germtz <ger.ms19@gmail.com>
2019-06-06 15:25:49 -0600

    formacobros

--- WcfErp/Servicios/PVenta/IWcfFormadeCobro.cs
+++ WcfErp/Servicios/PVenta/IWcfFormadeCobro.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using WcfErp.Modelos.PVenta;
 
 namespace WcfErp.Servicios.PVenta
 {
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IWcfFormadeCobro" en el código y en el archivo de configuración a la vez.
     [ServiceContract]
-    public interface IWcfFormadeCobro
+    public interface IWcfFormadeCobro : ServiciosBase<FormadeCobro>
     {
-        [OperationContract]
-        void DoWork();
+
     }
 }
