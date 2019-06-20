a7a72dc4a640a0ff03d680d9f8e57ee09022c1ce
Heriberto Lopez <heriberto@insolux.com>
2019-06-19 18:56:08 -0600

    municipios

--- WcfErp/Modelos/Generales/Municipio.cs
+++ WcfErp/Modelos/Generales/Municipio.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WcfErp.Modelos.Inventarios;
 
 namespace WcfErp.Modelos.Generales
 {
     public class Municipio : ModeloBase<Municipio>
     {
-
+        public Estado Estado { get; set; }
+        public Procedencia Procedencia { get; set; }
     }
 }
