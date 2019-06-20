d6493a219496d8b008e1e9513c69605300bcdba2
Daniel Cuellar <systhem@gmail.com>
2019-06-19 18:31:31 -0600

    Estado

---
+++ WcfErp/Modelos/Generales/Estado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WcfErp.Modelos.Inventarios;
+
+namespace WcfErp.Modelos.Generales
+{
+    public class Estado : ModeloBase<Estado>
+    {
+        public Procedencia Procedencia { get; set; }
+    }
+}
