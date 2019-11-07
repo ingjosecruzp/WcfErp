6017fc46ac47ffadd5cf54fd24face1e81d91058
Germtz <ger.ms19@gmail.com>
2019-11-06 17:48:59 -0700

    cambios

--- WcfErp/Servicios/Compras/WcfTipoProveedores.svc.cs
+++ WcfErp/Servicios/Compras/WcfTipoProveedores.svc.cs
@@ -12,7 +12,7 @@ namespace WcfErp.Servicios.Compras
 {
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "WcfTipoPoveedores" en el código, en svc y en el archivo de configuración a la vez.
     // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione WcfTipoPoveedores.svc o WcfTipoPoveedores.svc.cs en el Explorador de soluciones e inicie la depuración.
-    public class WcfTipoPoveedores : ServiceBase<TipoProveedor,EmpresaContext>, IWcfTipoProveedores
+    public class WcfTipoProveedores : ServiceBase<TipoProveedor,EmpresaContext>, IWcfTipoProveedores
     {
         public TipoProveedor delete(string id)
         {
