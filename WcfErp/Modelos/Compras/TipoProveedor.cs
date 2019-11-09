881cb9fcb0f8ef49d3f4a50627fd2f2bc232ddb7
Germtz <ger.ms19@gmail.com>
2019-11-08 17:41:34 -0700

    Todo De Nuevo

--- WcfErp/Modelos/Compras/TipoProveedor.cs
+++ WcfErp/Modelos/Compras/TipoProveedor.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace WcfErp.Modelos.Compras
 {
-    public class TipoProveedor: ModeloBase<TipoProveedor, EmpresaContext>
+    public class TipoProveedor : ModeloBase<TipoProveedor, EmpresaContext>
     {
         public string Clave { get; set; }
     }
